bc1bc54cf72b3f82fd1f452fd61a281784398ece
Juraj <jurosik5000@gmail.com>
2024-09-24 12:54:06 +0200

    NewSubpages

--- src/app/secret/page.tsx
+++
@@ -1,8 +0,0 @@
-// src/app/profile/[id]/page.tsx
-
-import { permanentRedirect } from 'next/navigation';
-
-
-export default function ComboBox() {
-    permanentRedirect("http://10.1.40.11:3000/secret")
-}
