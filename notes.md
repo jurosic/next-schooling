00386539c98a0c3bc0f6dcb8120c9ec975aa7e9a
Juraj <jurosik5000@gmail.com>
2024-10-08 10:56:49 +0200

    rewrote notes to md, added packages

---
+++ notes.md
@@ -0,0 +1,21 @@
+# Poznámky
+----
+### Meno: *Juraj Jánošík*
+### Trieda: *4.B - 1. sk*
+
+## Linux
+```bash
+clear; "Vyčistí textový buffer terminalového emulátora"
+ls [flags] [path]; "list dir"
+```
+
+## npm
+```bash
+npm [args]; "node package manager"
+```
+|arg|desc|
+|---|----|
+|run dev| spustí devel server|
+|run build| spustí produkčnú kompiláciu projektu|
+|run start| startne server z poslednej kompilácie|
+|create-next-app| urobí nový NEXT.js projekt|
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,27 @@
-
-
 import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
-export default function ComboBox() {
+export default function SimpleBottomNavigation() {
+  const [value, setValue] = React.useState(0);
+
   return (
-    <Container>
-        <Typography>Navbar</Typography>
-    </Container>
+    <Box sx={{ width: 500 }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      </BottomNavigation>
+    </Box>
   );
-}
+}x
