96843860cdd83ccd1ee6cf07323788a879d8f042
Juraj <jurosik5000@gmail.com>
2024-10-15 13:02:27 +0200

    changed navbar behavior

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,26 +5,41 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+
+import HomeIcon from '@mui/icons-material/Home';
+import PlusIcon from '@mui/icons-material/PlusOne';
+import LoginIcon from '@mui/icons-material/Login';
+import RegisterIcon from '@mui/icons-material/PersonAdd';
 
 export default function BarováNavigácia() {
   const [value, setValue] = React.useState(0);
+
+  const getValue = () => {
+    switch (window.location.pathname) {
+      case "/": return 0;
+      case "/post": return 1;
+      case "/auth/register": return 2;
+      case "/auth/login": return 3;
+      default: return 0;
+    }
+  }
+
+  console.log(getValue());
 
   return (
     <Box sx={{ width: 500 }}>
       <BottomNavigation
         showLabels
-        value={value}
+        value={getValue()}
         onChange={(event, newValue) => {
           setValue(newValue);
         }}
       >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-      </BottomNavigation>
+      <BottomNavigationAction label="Domov" icon={<HomeIcon />} onClick={() => { window.location.href = "/" }} />
+      <BottomNavigationAction label="Pridať" icon={<PlusIcon />} onClick={() => { window.location.href = "/post"; }} />
+      <BottomNavigationAction label="Registrácia" icon={<RegisterIcon />} onClick={() => { window.location.href = "/auth/register"; }} />
+      <BottomNavigationAction label="Prihlásenie" icon={<LoginIcon />} onClick={() => { window.location.href = "/auth/login"; }} />
+    </BottomNavigation>
     </Box>
   );
 }
